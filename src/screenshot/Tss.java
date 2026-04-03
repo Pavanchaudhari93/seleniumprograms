9cbbb8f991ac573d10be67c645f547df36d2eb69
Pavanchaudhari93 <chaudhari.pavan19@gmail.com>
2026-04-03 06:37:05 +0530

    uodate the code

--- src/screenshot/Tss.java
+++ src/screenshot/Tss.java
@@ -9,27 +9,26 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
 public class Tss {
-	
+
 	public static WebDriver driver;
 
 	public static void main(String[] args) throws IOException {
-		
-	System.setProperty("webdriver.chrome.driver", "/Users/pavanchaudhari/Downloads/chromedriver");
-		
-	driver=new ChromeDriver();
-	
-	driver.navigate().to("https://www.instagram.com/");
-	
-	TakesScreenshot ss=(TakesScreenshot)driver;
-	
-	File format=ss.getScreenshotAs(OutputType.FILE);
-	
-	FileUtils.copyFile(format, new File("/Users/pavanchaudhari/eclipse-workspace/A-SeleniumDemo/ss/instagram.jpg"));
-	
-	driver.close();
-	
+
+		System.setProperty("webdriver.chrome.driver", "/Users/pavanchaudhari/Downloads/chromedriver");
+
+		driver = new ChromeDriver();
+
+		driver.navigate().to("https://www.instagram.com/");
+
+		TakesScreenshot ss = (TakesScreenshot) driver;
+
+		File format = ss.getScreenshotAs(OutputType.FILE);
+
+		FileUtils.copyFile(format, new File("/Users/pavanchaudhari/eclipse-workspace/A-SeleniumDemo/ss/instagram.jpg"));
+
+		driver.close();
+
 	}
 
 }
